96264c913e7788edb3704193b25da77157c23c33
sevvalyldrrm <ysevvalyldrm@gmail.com>
2023-02-27 15:58:09 +0300

    fetch method

---
+++ src/hooks/useFetch/useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
+
+function useFetch(url){
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+
+   const fetchData = async () => {
+    try {
+      const {data: responseData} = await axios.get(url);
+     setData(responseData);
+     setLoading(false);
+    } catch(error) {
+      setError (error.message);
+      setLoading (false);
+    }
+   };
+
+   useEffect(() => {
+    fetchData()
+   }, []);
+
+  // useEffect(() => {
+  //   fetch(REACT_APP_DENEME)
+  //     .then((resp) => resp.json())
+  //     .then((json) => setData(json))
+  //     .catch((error) => console.error(error)) 
+  //     .finally(() => setLoading(false));
+  // }, []);
+
+   return {error, loading, data};
+}
+
+export default useFetch;
--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,38 +1,15 @@
 import { View, Text, FlatList, ActivityIndicator } from 'react-native'
 import React, {useState, useEffect} from 'react'
 import {REACT_APP_DENEME} from '@env';
-import axios from 'axios'
+
 
 import ProductCard from '../../components/ProductCard'
+import useFetch from '../../hooks/useFetch';
 
 export default function Product() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-    useEffect(() => {
-    fetchData();
-   }, []);
+  const {loading, data, error} = useFetch(REACT_APP_DENEME);
 
-   const fetchData = async () => {
-    try {
-      const {data: productData} = await axios.get(REACT_APP_DENEME);
-     setData(productData);
-     setLoading(false)
-    } catch(error) {
-      setError (error.message)
-      setLoading (false)
-    }
-   };
-
-
-  // useEffect(() => {
-  //   fetch(REACT_APP_DENEME)
-  //     .then((resp) => resp.json())
-  //     .then((json) => setData(json))
-  //     .catch((error) => console.error(error)) 
-  //     .finally(() => setLoading(false));
-  // }, []);
 
   const renderProduct = ({item}) => <ProductCard product={item}/>;
 
@@ -43,9 +20,10 @@ export default function Product() {
   if(error){
     return <Text>{error}</Text>
   }
+
   return (
     <View>
       <FlatList data = {data} renderItem={renderProduct}/>
     </View>
-  )
-}
+  );
+};
